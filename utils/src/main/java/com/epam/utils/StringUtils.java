d3689c555a1608d88188fb6cb9eeeab416d91a5b
Welkus <vdelkus@gmail.com>
2023-11-07 21:46:39 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	demo/build.gradle
    #	utils/build.gradle

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+
         try {
-            return args.stream().map(Double::parseDouble).allMatch(n -> n > 0);
+            return Integer.parseInt(str) > 0;
 
         } catch (NullPointerException e) {
             return false;
